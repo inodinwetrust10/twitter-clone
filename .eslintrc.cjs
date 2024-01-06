be098f2cbf644a195ac377e3818a24ebe7f898bc
inodinwetrust10 <adi4gbsingh@gmail.com>
2024-01-06 14:46:07 +0530

    eslint changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,8 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/prefer-optional-chain": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
